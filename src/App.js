569f92d32c31c07d0849724cd68697f91aa0938f
Ricard Codina <58882113+miuet@users.noreply.github.com>
2024-05-24 18:13:13 +0200

    App Crossword

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import Crossword from "@jaredreisinger/react-crossword";
 
+const data = {
+  across: {
+    1: {
+      clue: "mots",
+      answer: "MOTS",
+      row: 0,
+      col: 0,
+    },
+  },
+  down: {
+    2: {
+      clue: "berguedans al revés",
+      answer: "SNADEGUEB",
+      row: 0,
+      col: 3,
+    },
+  },
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Crossword data={data} />;
     </div>
   );
 }
