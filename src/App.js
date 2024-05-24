d55b85bb21db7cfd6e90be30581586e83c2ff7ad
Ricard Codina <58882113+miuet@users.noreply.github.com>
2024-05-24 18:26:25 +0200

    canvis mots

--- src/App.js
+++ src/App.js
@@ -6,25 +6,53 @@ import Crossword from "@jaredreisinger/react-crossword";
 const data = {
   across: {
     1: {
-      clue: "mots",
-      answer: "MOTS",
+      clue: "PISTA",
+      answer: "PARAULA",
       row: 0,
       col: 0,
     },
+    5: {
+      clue: "PISTA2",
+      answer: "PARAULA2",
+      row: 2,
+      col: 0,
+    },
+    6: {
+      clue: "PISTA3",
+      answer: "PARAULA3",
+      row: 4,
+      col: 0,
+    },
+    // Add more words here
   },
   down: {
     2: {
-      clue: "berguedans al revés",
-      answer: "SNADEGUEB",
+      clue: "PISTA4",
+      answer: "PARAULA4",
       row: 0,
-      col: 3,
+      col: 2,
     },
+    3: {
+      clue: "PISTA5",
+      answer: "PARAULA5",
+      row: 0,
+      col: 4,
+    },
+    4: {
+      clue: "PISTA6",
+      answer: "PARAULA6",
+      row: 0,
+      col: 6,
+    },
+    // Add more words here
   },
 };
 function App() {
   return (
     <div className="App">
-      <Crossword data={data} />;
+      <div className="encreuats">
+        <Crossword data={data} />;
+      </div>
     </div>
   );
 }
